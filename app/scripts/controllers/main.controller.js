be7f1832c81263a380bd9e9565fba9e2ef45526e
Ed Gilmore <edgilmore@live.com>
2016-03-04 09:44:04 -0700

    moved marker loop inside of data check

--- app/scripts/controllers/main.controller.js
+++ app/scripts/controllers/main.controller.js
@@ -30,16 +30,16 @@
                             scrollwheel:true,
                             draggable: true
                         };
-                    }
-                    for(var i = 0; i < vm.foodtrucks.length; i++){
-                        if(vm.foodtrucks[i].location != undefined && vm.foodtrucks[i].objectid != undefined){
-                            var markerObject = {
-                                id: vm.foodtrucks[i].objectid,
-                                latitude: vm.foodtrucks[i].location.latitude,
-                                longitude: vm.foodtrucks[i].location.longitude,
-                                title: vm.foodtrucks[i].applicant
-                            };
-                            vm.markers.push(markerObject)
+                        for(var i = 0; i < vm.foodtrucks.length; i++){
+                            if(vm.foodtrucks[i].location != undefined && vm.foodtrucks[i].objectid != undefined){
+                                var markerObject = {
+                                    id: vm.foodtrucks[i].objectid,
+                                    latitude: vm.foodtrucks[i].location.latitude,
+                                    longitude: vm.foodtrucks[i].location.longitude,
+                                    title: vm.foodtrucks[i].applicant
+                                };
+                                vm.markers.push(markerObject)
+                            }
                         }
                     }
                 });
